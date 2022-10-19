d1b1bc9021373fc36561ee011005a54b150e813f
José Inácio <joseinacio2001.jic@gmail.com>
2022-10-19 09:06:34 -0300

    ModalLogin

--- front-end/src/components/organisms/AppModalLogin.jsx
+++ front-end/src/components/organisms/AppModalLogin.jsx
@@ -12,16 +12,41 @@ export default function AppModalLogin(props) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
+            Identifique-se
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
+        <div className="modal-body p-5 pt-0">
+  <form className="">
+    <div className="form-floating mb-3">
+      <input
+        type="email"
+        className="form-control rounded-3"
+        id="floatingInput"
+        placeholder="name@example.com"
+      />
+      <label htmlFor="floatingInput">Email</label>
+    </div>
+    <div className="form-floating mb-3">
+      <input
+        type="password"
+        className="form-control rounded-3"
+        id="floatingPassword"
+        placeholder="Password"
+      />
+      <label htmlFor="floatingPassword">Senha</label>
+    </div>
+    <button
+      className="w-100 mb-2 btn btn-lg rounded-3 btn-primary"
+      type="submit"
+    >
+      ENTRAR
+    </button>
+    <small className="text-muted">
+      Ao clicar em ENTRAR estará aceitando os Termos de uso.
+    </small>
+  </form>
+</div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
