a4b2a8efe293f5082a65366650f00cdac982ef8c
José Inácio <joseinacio2001.jic@gmail.com>
2022-10-19 08:47:11 -0300

    Botao de entrar e modal

--- front-end/src/components/organisms/AppLoading.jsx
+++ front-end/src/components/organisms/AppLoading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
 
-export default function LinearDeterminate() {
+export default function AppLoading() {
   const [progress, setProgress] = React.useState(0);
 
   React.useEffect(() => {
