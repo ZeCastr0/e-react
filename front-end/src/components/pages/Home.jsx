87c7a6354a62b7932130b25285c98e4db603b052
José Inácio <103376174+ZeCastr0@users.noreply.github.com>
2022-10-27 19:53:10 -0300

    modified:   front-end/src/components/organisms/AppLoading.jsx 	modified:   front-end/src/components/pages/Home.jsx

--- front-end/src/components/pages/Home.jsx
+++ front-end/src/components/pages/Home.jsx
@@ -4,13 +4,6 @@ import AppCards from "../organisms/AppCards";
 import Carousel from "../organisms/Carousel";
 import Footer from "../organisms/Footer";
 
-/* React.useEffect(() => {
-    fetch("")
-      .then((response) => response.json())
-      .then((data) => {   
-      });
-  }, []); CONSUMINDO API */
-
 export default function Home() {
   return (
     <div>
