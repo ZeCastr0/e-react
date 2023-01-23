90ce8e346444110f2b2147cd9024e9bdb258ba27
José Inácio <103376174+ZeCastr0@users.noreply.github.com>
2023-01-23 12:25:18 -0300

    modified:   back-end/src/routes/Routes.js

--- back-end/src/routes/Routes.js
+++ back-end/src/routes/Routes.js
@@ -5,21 +5,10 @@ const router = express.Router();
 
 const controllerProcs = require("../controllers/ControllerProcs"); // indica onde esta o arquivo de controllers
 
-//const authController = require("../controllers/authController"); // indica onde esta o arquivo de controllers
-//
-//router.get("/all", controller.getAll); // rota para o controller para listar todos os usuraios
-//router.get("/user/:id", controller.getById); // rota para o controller para listar  usuraios por id
-//router.post("/create", controller.createUser); // rota para o controller para criar usuraios
-//router.patch("/update/:id", controller.updateUserById); // rota para o controller para atualizar usuraios pelo id
-//router.delete("/delete/:id", controller.deleteUserById); // rota para o controller para deletar usuraios pelo ID
-//
-//
-//router.post("/login", authController.login); // rota para o controller para fazer login
+router.post("/login", controllerProcs.login);
+router.post("/cadastro", controllerProcs.cadastro);
+router.post("/addcarrinho", controllerProcs.addCarrinho);
+router.get("/selectcarrinho", controllerProcs.selectCarrinho);
+router.post("/addproduto", controllerProcs.addProduto);
 
 module.exports = router; // deixa o arquivo despinivel para ser usado
-
-//router.post("/login",
-//router.post("/cadastro",
-//router.post("/addcarrinho",
-//router.get("/selectcarrinho",
-//router.post("/addproduto",
