d1a2522bb0402f6235522a419a74b9bee64e047f
José Inácio <103376174+ZeCastr0@users.noreply.github.com>
2023-01-22 22:12:56 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   back-end/.env.example
    	modified:   back-end/src/app.js
    	modified:   back-end/src/config/database.js
    	new file:   back-end/src/config/procedures.js
    	new file:   back-end/src/config/table.js
    	new file:   back-end/src/controllers/execProcs.js
    	deleted:    back-end/src/controllers/userController.js
    	modified:   back-end/src/routes/userRoutes.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,17 +1,16 @@
-//arquivo responsavel por impotar as bibliotecas baixadas com o NPM e declarar a rota principal
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-//app.use("/users", userRoutes);
 
 require("dotenv-safe").config();
 
-const db = require("./config/database");
-const userRoutes = require("./routes/userRoutes");
+const { createTables } = require("./config/table");
+const { createProcedures } = require("./config/procedures");
+createTables();
+createProcedures();
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -1,10 +1,10 @@
-//// Arquivo responsal por gerenciar as rotas
-//
-//const express = require("express");
-//const router = express.Router();
-//
-//const controller = require("../controllers/userController"); // indica onde esta o arquivo de controllers
-//
+// Arquivo responsal por gerenciar as rotas
+
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controllers/execProcs"); // indica onde esta o arquivo de controllers
+
 //const authController = require("../controllers/authController"); // indica onde esta o arquivo de controllers
 //
 //router.get("/all", controller.getAll); // rota para o controller para listar todos os usuraios
@@ -13,6 +13,12 @@
 //router.patch("/update/:id", controller.updateUserById); // rota para o controller para atualizar usuraios pelo id
 //router.delete("/delete/:id", controller.deleteUserById); // rota para o controller para deletar usuraios pelo ID
 //
-//router.post("/login", authController.login); // rota para o controller para fazer login
 //
-//module.exports = router; // deixa o arquivo despinivel para ser usado
+//router.post("/login", authController.login); // rota para o controller para fazer login
+
+router.post("/create-produtos", (req, res) => {
+  controller.createTable();
+  res.send("Tabela criada com sucesso");
+});
+
+module.exports = router; // deixa o arquivo despinivel para ser usado
